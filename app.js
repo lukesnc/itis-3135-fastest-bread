011150a8e4134794785f6f781025710a85a48fe7
lukesnc <lukeasimone@gmail.com>
2023-11-18 13:54:07 -0500

    carousel done

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 "use strict";
 
-const gallery = [
-  {
-    path: "img/baguettes.webp",
-    text: "Fresh Baguettes on Thursday",
-  },
-  {},
-  {},
-  {},
-];
+const previewItems = Array.from(document.querySelectorAll(".preview-item"));
+const previewCaption = document.querySelector(".preview-caption");
+const arrows = Array.from(document.querySelectorAll(".preview-arrow"));
+const PREVIEW_SIZE = previewItems.length;
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", swipe);
+});
+
+function swipe(e) {
+  const currentPreviewlItem = document.querySelector(".preview-item.active");
+  const currentIndex = previewItems.indexOf(currentPreviewlItem);
+
+  let nextIndex;
+
+  if (e.currentTarget.classList.contains("left")) {
+    if (currentIndex === 0) {
+      nextIndex = PREVIEW_SIZE - 1;
+    } else {
+      nextIndex = currentIndex - 1;
+    }
+  } else {
+    if (currentIndex === PREVIEW_SIZE - 1) {
+      nextIndex = 0;
+    } else {
+      nextIndex = currentIndex + 1;
+    }
+  }
+
+  previewItems[nextIndex].classList.add("active");
+  currentPreviewlItem.classList.remove("active");
+  previewCaption.textContent =
+    previewItems[nextIndex].firstElementChild.getAttribute("alt");
+}
