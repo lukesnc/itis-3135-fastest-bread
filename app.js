08f098ebe210008e24ff2532d761c20eedf9faeb
lukesnc <lukeasimone@gmail.com>
2023-11-19 17:28:12 -0500

    testimonials transition

--- app.js
+++ app.js
@@ -34,3 +34,34 @@ function swipe(e) {
   previewCaption.textContent =
     previewItems[nextIndex].firstElementChild.getAttribute("alt");
 }
+
+const testimonials = Array.from(document.querySelectorAll(".testimonial"));
+
+testimonials.forEach((t) => {
+  t.addEventListener("click", clickTestimonial);
+  if (t.lastElementChild.classList.contains("open")) {
+    t.style.setProperty("width", "300px");
+    t.style.setProperty("height", "250px");
+  }
+});
+
+function clickTestimonial(e) {
+  e.currentTarget.lastElementChild.classList.toggle("open");
+  e.currentTarget.children[1].classList.toggle("open");
+  if (e.currentTarget.lastElementChild.classList.contains("open")) {
+    e.currentTarget.style.setProperty("width", "300px");
+    e.currentTarget.style.setProperty("height", "250px");
+  } else {
+    e.currentTarget.style.setProperty("width", "120px");
+    e.currentTarget.style.setProperty("height", "120px");
+  }
+
+  testimonials.forEach((t) => {
+    if (e.currentTarget !== t) {
+      t.children[1].classList.add("open");
+      t.lastElementChild.classList.remove("open");
+      t.style.setProperty("width", "120px");
+      t.style.setProperty("height", "120px");
+    }
+  });
+}
